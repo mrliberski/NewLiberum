78612b88774029b63678f81e52b454e757f2b5da
Pawel Liberski <panliberski@outlook.com>
2023-05-21 07:34:53 +0100

    Excel logic extracted to library

--- Liberum/ProFormaLibrary/About.Designer.cs
+++ Liberum/ProFormaLibrary/About.Designer.cs
@@ -178,13 +178,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
-        private System.Windows.Forms.PictureBox logoPictureBox;
-        private System.Windows.Forms.Label labelProductName;
-        private System.Windows.Forms.Label labelVersion;
-        private System.Windows.Forms.Label labelCopyright;
-        private System.Windows.Forms.Label labelCompanyName;
-        private System.Windows.Forms.TextBox textBoxDescription;
-        private System.Windows.Forms.Button okButton;
+        private TableLayoutPanel tableLayoutPanel;
+        private PictureBox logoPictureBox;
+        private Label labelProductName;
+        private Label labelVersion;
+        private Label labelCopyright;
+        private Label labelCompanyName;
+        private TextBox textBoxDescription;
+        private Button okButton;
     }
 }
